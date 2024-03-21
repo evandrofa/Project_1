d8b1b2cec30aeb815a63c366f152b9c5799f808b
Evandro <evandro.almeida@live.com>
2024-03-21 14:43:46 -0300

    att

--- cotacoes.py
+++ cotacoes.py
@@ -2,6 +2,7 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import indices
 
 browser = webdriver.Chrome()
 url = 'https://www.noticiasagricolas.com.br/cotacoes/frutas'
@@ -10,7 +11,7 @@ browser.get(url)
 text_list_preco = []
 text_list_praca = []
 
-for i in range(1, 10):
+for i in range(indices.maca_fuji + 1):
     lista_preco = browser.find_elements(By.CSS_SELECTOR, f'.tables .cotacao .table-content .cot-fisicas tbody tr:nth-child({i}) td:nth-child(2)')
     lista_pracas = browser.find_elements(By.CSS_SELECTOR, f'.tables .cotacao .table-content .cot-fisicas tbody tr:nth-child({i}) td:nth-child(1)')
     text_list_preco.append([texto.text for texto in lista_preco])
@@ -25,13 +26,15 @@ df = pd.DataFrame({'Preço': [preco for sublist in text_list_preco for preco in 
 resumo = 'dados_frutas.csv'
 df.to_csv(resumo, index=False)
 
-
 #let test = document.querySelector(`div:nth-child(4)  div.info  h2  a`).innerText
 #console.log(test)
 
 abacate = []
 for x in text_list_praca:
-    abacate.append(x[0])
-
+    try:
+        abacate.append(x[indices.maca_fuji_indice])
+    except:
+        pass
+    
 for y in abacate:
     print(y)
---
+++ indices.py
@@ -0,0 +1,25 @@
+abacate = 9
+abacaxi = 6
+mamao = 9
+maracuja = 9
+pera = 9
+tangerina = 9
+uva = 9
+melancia = 12
+banana = 9
+limao = 11
+maca_fuji = 3
+maca_gala = 3
+
+abacate_indice = 0
+abacaxi_indice = 1
+mamao_indice = 2
+maracuja_indice = 3
+pera_indice = 4
+tangerina_indice = 5
+uva_indice = 6
+melancia_indice = 7
+banana_indice = 8
+limao_indice = 9
+maca_fuji_indice = 10
+maca_gala_indice = 11
