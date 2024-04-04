37d26d2e908ed4c55a37b00c2aff5a6243492865
evandro <evandro.almeida@live.com>
2024-04-04 14:03:09 -0300

    just saving

--- cotacoes.py
+++ cotacoes.py
@@ -17,11 +17,12 @@ url = 'https://www.noticiasagricolas.com.br/cotacoes/frutas'
 browser.get(url)
 
 
-txt_lista_nomes = []
-txt_lista_precos = []
-fruta = []
+# txt_lista_nomes = []
+# txt_lista_precos = []
 
 def search_bot(tam:int,id:int):
+    txt_lista_nomes = []
+    txt_lista_precos = []
     for i in range(1,tam):
         pracas_nomes = browser.find_elements(By.CSS_SELECTOR, f'table#\\"anchor\\"{id}.cot-fisicas tbody tr:nth-child({i}) td:nth-child(1)')
         precos_produto = browser.find_elements(By.CSS_SELECTOR, f'table#\\"anchor\\"{id}.cot-fisicas tbody tr:nth-child({i}) td:nth-child(2)') 
@@ -29,28 +30,23 @@ def search_bot(tam:int,id:int):
         txt_lista_precos.append([elemento.text for elemento in precos_produto])
     return txt_lista_nomes, txt_lista_precos
 
-# abacate = search_bot(10, 189)
-# abacaxi = search_bot(7, 190)
-# mamao = search_bot(10 ,191)
-# maracuja = search_bot(10, 192)
-# pera = search_bot(10, 193)
-# tangerina = search_bot(10, 195)
-# uva = search_bot(10, 194)
-# melancia = search_bot(13, 89)
-# banana_nanica_prata = search_bot(10, 68)
-# limao_tahiti = search_bot(12, 72)
-# maca_fuji = search_bot(4, 73)
-# maca_gala = search_bot(4, 74)
-
-pera_praca, pera_valor = search_bot(10, 193)
-print(pera_praca)
-print(pera_valor)
-
+abacate_local, abacate_preco = search_bot(10, 189)
+abacaxi_local, abacaxi_preco = search_bot(7, 190)
+mamao_local, mamao_preco = search_bot(10 ,191)
+maracuja_local, maracuja_preco = search_bot(10, 192)
+pera_local, pera_preco = search_bot(10, 193)
+tangerina_local, tangerina_preco = search_bot(10, 195)
+uva_local, uva_preco = search_bot(10, 194)
+melancia_local, melancia_preco = search_bot(13, 89)
+banana_nanica_prata_local, banana_nanica_prata_preco = search_bot(10, 68)
+limao_tahiti_local, limao_tahiti_preco = search_bot(12, 72)
+maca_fuji_local, maca_fuji_preco = search_bot(4, 73)
+maca_gala_local, maca_gala_preco = search_bot(4, 74)
 
 
 df = pd.DataFrame({
-                'Pera Praça': [pera for sublist in pera_praca for pera in sublist],
-                'Pera Preço': [pera_valor for sublist in pera_valor for pera_valor in sublist]
+                'Pera Praça': abacate_local,
+                'Pera Preço': abacate_preco
                 })
 
 # Salvar o DataFrame como um arquivo CSV
